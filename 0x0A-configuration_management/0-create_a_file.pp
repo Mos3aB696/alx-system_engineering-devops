d142e3f2d416cb3d0f707b3881cb0b9680f0b481
Mos3aB696 <mos3ab696@gmail.com>
2024-01-20 02:24:33 +0200

    Ta_0_sk

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@ file { '/tmp/school':
   ensure  => file,
   path    => '/tmp/school',
   mode    => '0744',
-  owner   => 'WWW-data',
-  group   => 'WWW-data',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
