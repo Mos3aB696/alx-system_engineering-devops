ad47c4d7c3d7a386fe8febd0a9f861a4a3bb9934
Mos3aB696 <mos3ab696@gmail.com>
2024-02-19 20:52:31 +0200

    Ta_0_sk

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-"""
-Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python3  0-gather_data_from_an_API.py EMPLOYEE_ID")
-        exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    employee_id = argv[1]
-
-    # Make a GET request to retrieve employee information
-    response_user = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-    user_data = response_user.json()
-    employee_name = user_data.get('name')
-
-    # Make a GET request to retrieve employee's TODO list
-    response_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'
-        .format(employee_id))
-    todos_data = response_todos.json()
-
-    # Calculate the number of completed tasks
-    completed_tasks = [task for task in todos_data if task.get('completed')]
-    number_of_done_tasks = len(completed_tasks)
-    total_number_of_tasks = len(todos_data)
-
-    # Print the employee TODO list progress
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          number_of_done_tasks, total_number_of_tasks))
-    for task in completed_tasks:
-        print("\t{}".format(task.get('title')))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
