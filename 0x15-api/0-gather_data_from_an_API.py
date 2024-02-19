b45bea4af86456c1cb13824f1c4f73cd89aa69d8
Mos3aB696 <mos3ab696@gmail.com>
2024-02-19 20:47:26 +0200

    Ta_0_sk

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,8 @@ Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
-import requests
 from sys import argv
+import requests
 
 if __name__ == "__main__":
     if len(argv) != 2:
