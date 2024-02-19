9a3b4a6934a99c0d502e675c1f10a4867e515499
Mos3aB696 <mos3ab696@gmail.com>
2024-02-20 00:21:12 +0200

    Ta_3_sk

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,12 +7,16 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as json_f:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, json_f)
+    the_list = {}
+    for user in users:
+        user_id = user.get("id")
+        todos = requests.get(url + "todos", params={"userId": user_id}).json()
+        the_list[user_id] = []
+        for todo in todos:
+            the_list[user_id].append({
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username")
+            })
+        with open("todo_all_employees.json", "w") as json_f:
+            json.dump(the_list, json_f)
