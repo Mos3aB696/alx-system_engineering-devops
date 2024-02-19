849104f50274c7072896a33d90d51ba59a554203
Mos3aB696 <mos3ab696@gmail.com>
2024-02-19 22:44:36 +0200

    Ta_0_sk

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
+    user_id = argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     completed = []
     for todo in todos:
