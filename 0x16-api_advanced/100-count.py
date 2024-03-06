1c41308ab98f9488a13b08930afe85c4c6eb42bd
Mos3aB696 <mos3ab696@gmail.com>
2024-03-06 02:38:30 +0200

    Adv

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,13 +3,17 @@
 import requests
 
 
+#!/usr/bin/python3
+"""Function to count words in all hot posts of a given Reddit subreddit."""
+import requests
+
 def count_words(subreddit, word_list, instances={}, after="", count=0):
     """Prints counts of given words found in hot posts of a given subreddit.
 
     Args:
         subreddit (str): The subreddit to search.
         word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
+        instances (dict): Key/value pairs of words/counts.
         after (str): The parameter for the next page of the API results.
         count (int): The parameter of results matched thus far.
     """
@@ -40,10 +44,10 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         for word in word_list:
             if word.lower() in title:
                 times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
+                if instances.get(word.lower()) is None:
+                    instances[word.lower()] = times
                 else:
-                    instances[word] += times
+                    instances[word.lower()] += times
 
     if after is None:
         if len(instances) == 0:
