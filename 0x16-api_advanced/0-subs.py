0b513a30e8c803383ca4bf370506d0aa68c0a687
Mos3aB696 <mos3ab696@gmail.com>
2024-04-09 19:11:27 +0200

    Ta_0_sk

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,13 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        results = response.json().get("data")
-        if results.get("subscribers") is not None:
-            return results.get("subscribers")
-    return 0
+    if response.status_code == 404:
+        return 0
+
+    results = response.json().get("data")
+    return results.get("subscribers")
+    # if response.status_code == 200:
+    #     results = response.json().get("data")
+    #     if results.get("subscribers") is not None:
+    #         return results.get("subscribers")
+    # return 0
