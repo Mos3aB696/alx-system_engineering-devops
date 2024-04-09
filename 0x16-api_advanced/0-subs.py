97ec1bafb63089313b0e0338b865b87ca0140ec0
Mos3aB696 <mos3ab696@gmail.com>
2024-04-09 19:46:40 +0200

    Ta_0_sk

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    (total subscribers) for a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
-    base_url = "https://www.reddit.com/r/"
-    url = f"{base_url}{subreddit}/about.json"
-
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Custom User Agent (your choice)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        return data["data"]["subscribers"]
-    except (requests.RequestException, KeyError):
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        if results.get("subscribers") is not None:
+            return results.get("subscribers")
+    else:
         return 0
